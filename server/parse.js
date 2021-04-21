ab9dc0ed792b2f83f328ed7af0aa11d2237277bc
Eric Feige-Gernoth <ericfg@protonmail.com>
2021-04-21 17:26:04 +0200

    update

--- server/parse.js
+++ server/parse.js
@@ -1,7 +1,23 @@
 console.log("Hello i am here");
 var convert = require("xml-js");
 const fs = require("fs");
-var xml = fs.readFileSync("exam.xml", "utf8");
-var options = { ignoreComment: true, alwaysChildren: true };
-var result = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
-console.log(result);
+var DOMParser = require("xmldom").DOMParser;
+var parseString = require("xml2js").parseString;
+var xml = fs.readFileSync(__dirname + "/dic.xml", "utf8");
+
+var xmlStringSerialized = new DOMParser().parseFromString(xml, "text/xml");
+parseString(xmlStringSerialized, function (err, result) {
+    if (err) {
+        //did not work
+        // console.log(err);
+    } else {
+        //worked! use JSON.stringify()
+        var allDone = JSON.stringify(result);
+        fs.writeFileSync(`files.json`, allDone);
+    }
+});
+
+// var options = { ignoreComment: true, alwaysChildren: true };
+// var result = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
+// console.log(result);
+// console.log(__dirname);
